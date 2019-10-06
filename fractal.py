9bcb97d0a497cb956a1126dbae817945da262b19
clem <127137+7clem@users.noreply.github.com>
2019-10-06 18:56:55 +0200

    removed class and used globals

--- fractal.py
+++ fractal.py
@@ -20,64 +20,51 @@ def ratio(start, value, width):
     return (value - start) / width
 
 
-screen = None
-pxArray = None
+def gameLoop():
+    """Start game loop"""
+           #   ... leave game loop
+    while True:
+        _listen_for_event()
+        mandelbrot(space, view, pxArray)
+
+def pixelToComplex(self, x, y):
+    px = x / view.width * space.width + space.left
+    py = y / view.height * space.height + space.top
+    return complex(px, py)
+
+def mandelbrot(space, view, pxArray):
+    p_min = 0
+    p_max = 0
+
+    for i in range(view.left, view.left + view.width):
+        for j in range(view.top, view.top + view.height):
+            c = self.pixelToComplex(i, j)
+            p = suite(c)
+            p_min = min(p, p_min)
+            p_max = max(p, p_max)
+            col = pygame.color.Color(0)
+            col.hsva = (p, 100, 100, 100)
+            pxArray[i, j] = col
+        pygame.display.update([pygame.Rect(i, 0, 1, self.view.height)])
 
 
-class Game:
-    """Responsible for the pygame stuff and draw our fractal."""
+def _listen_for_event():
+    """Listens for game events like key presses etc"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            _handle_exit()
 
-    def __init__(self, view):
-
-        pygame.event.pump()
-        # pygame.display.update()
-
-        self.view = view
-        screen = pygame.display.set_mode((self.view.width, self.view.height))
-        pxArray = pygame.PixelArray(screen)
-        self.space = pygame.Rect(-3, -2, 4, 4)
-        pygame.display.set_caption("Fractal")
-
-    def run(self):
-        """Start game loop"""
-               #   ... leave game loop
-        while True:
-            self._listen_for_event()
-            self._update_screen()
-
-    def pixelToComplex(self, x, y):
-        px = x / self.view.width * self.space.width + self.space.left
-        py = y / self.view.height * self.space.height + self.space.top
-        return complex(px, py)
-
-    def _update_screen(self):
-        p_min = 0
-        p_max = 0
-        for i in range(self.view.left, self.view.left + self.view.width):
-            for j in range(self.view.top, self.view.top + self.view.height):
-                c = self.pixelToComplex(i, j)
-                p = suite(c)
-                p_min = min(p, p_min)
-                p_max = max(p, p_max)
-                col = pygame.color.Color(0)
-                col.hsva = (p, 100, 100, 100)
-                pxArray[i, j] = col
-            pygame.display.update([pygame.Rect(i, 0, 1, self.view.height)])
-
-
-    def _listen_for_event(self):
-        """Listens for game events like key presses etc"""
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self._handle_exit()
-
-    def _handle_exit(self):
-        pygame.quit()
-        sys.exit(1)
+def _handle_exit(self):
+    pygame.quit()
+    sys.exit(1)
 
 
 if __name__ == "__main__":
     pygame.init()
+    self = pygame.Rect(-3, -2, 4, 4)
     view = pygame.Rect(0, 0, 512, 512)
-    game = Game(view)
-    game.run()
+    screen = pygame.display.set_mode((view.width, view.height))
+    pxArray = pygame.PixelArray(screen)
+    pygame.display.set_caption("Fractal")
+
+    gameLoop(view, space, pxArray)
